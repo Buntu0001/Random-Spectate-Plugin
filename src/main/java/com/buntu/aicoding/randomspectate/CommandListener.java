a12fc278fb7c428c62b934ae79f5023022a9183a
Buntu <57615703+Buntu0001@users.noreply.github.com>
2021-11-18 03:29:15 +0900

    fix some codes

--- src/main/java/com/buntu/aicoding/randomspectate/CommandListener.java
+++ src/main/java/com/buntu/aicoding/randomspectate/CommandListener.java
@@ -21,7 +21,7 @@ public class CommandListener implements CommandExecutor {
         Player target_player = player_spectate_state.get(player).spectated_player;
         player.sendMessage(AlterColorCode(String.format("&e[관전] &a%s&f님으로부터의 관전을 종료합니다.", target_player.getDisplayName())));
 
-        target_player.sendMessage(AlterColorCode(String.format("&e[관전] &a%s&f님으로부터의 관전이 종료되었습니다.", player.getDisplayName())));
+        target_player.sendMessage(AlterColorCode(String.format("&e[관전] &c%s&f님으로부터의 관전이 종료되었습니다.", player.getDisplayName())));
         player.setSpectatorTarget(null);
         player.setGameMode(GameMode.CREATIVE);
         player.teleport(player_spectate_state.get(player).spectating_player_previous_location);
@@ -35,8 +35,8 @@ public class CommandListener implements CommandExecutor {
         player.teleport(target_location);
         player.setSpectatorTarget(target_player);
         player.sendMessage(AlterColorCode(String.format("&e[관전] &a%s&f님의 관전을 시작합니다.", target_player.getDisplayName())));
-        player.sendMessage(AlterColorCode(String.format("&e[관전] &f관전을 종료하고 싶다면 &6[/관전 종료] &f를 입력해주세요.", target_player.getDisplayName())));
-        target_player.sendMessage(AlterColorCode(String.format("&e[관전] &f당신은 &a%s&f님으로부터 관전되고 있습니다.", player.getDisplayName())));
+        player.sendMessage(AlterColorCode(String.format("&e[관전] &f관전을 종료하고 싶다면 &6[/관전 종료] &f를 입력해 주세요.", target_player.getDisplayName())));
+        target_player.sendMessage(AlterColorCode(String.format("&e[관전] &f당신은 &c%s&f님으로부터 관전되고 있습니다.", player.getDisplayName())));
         if (player_spectate_state.containsKey(player)) {
             RemoveSpectate(player);
         }
@@ -49,43 +49,47 @@ public class CommandListener implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
         //player.sendMessage(AlterColorCode("&e[관전] &f랜덤한 플레이어를 관전합니다."));
-        ArrayList<Player> player_list = new ArrayList<>();
         if (label.equalsIgnoreCase("관전")) {
-            if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("종료")) {
-                    if (player_spectate_state.containsKey(player)) {
-                        if (player_spectate_state.get(player).spectating_player == player) {
-                            RemoveSpectate(player);
-                            return true;
+            if (player.isOp()) {
+                if (args.length == 1) {
+                    if (args[0].equalsIgnoreCase("종료")) {
+                        if (player_spectate_state.containsKey(player)) {
+                            if (player_spectate_state.get(player).spectating_player == player) {
+                                RemoveSpectate(player);
+                                return true;
+                            } else {
+                                player.sendMessage(AlterColorCode("&e[관전] &f당신은 관전 중이 아닙니다."));
+                                return true;
+                            }
                         } else {
                             player.sendMessage(AlterColorCode("&e[관전] &f당신은 관전 중이 아닙니다."));
                             return true;
                         }
                     } else {
-                        player.sendMessage(AlterColorCode("&e[관전] &f당신은 관전 중이 아닙니다."));
+                        if (args[0].equalsIgnoreCase(player.getName())) {
+                            player.sendMessage(AlterColorCode("&e[관전] &f자기 자신을 관전할 수 없습니다."));
+                            return true;
+                        }
+                        Player target_player = Bukkit.getPlayer(args[0]);
+                        Location previous_location = player.getLocation();
+                        SpectatePlayer(player, target_player, previous_location);
                         return true;
                     }
                 } else {
-                    if (args[0].equalsIgnoreCase(player.getName())) {
-                        player.sendMessage(AlterColorCode("&e[관전] &f자기 자신을 관전할 순 없습니다."));
-                        return true;
-                    }
-                    Player target_player = Bukkit.getPlayer(args[0]);
+                    ArrayList<Player> player_list = new ArrayList<>();
+                    player_list.addAll(Bukkit.getOnlinePlayers());
+                    player_list.remove(player);
+                    int player_count = player_list.size();
+                    Random random = new Random();
+                    Player target_player = player_list.get(random.nextInt(player_count));
+                    //player.sendMessage(AlterColorCode(String.format("&e[관전] &a%s&f님이 걸렸습니다!", target_player.getDisplayName())));
                     Location previous_location = player.getLocation();
                     SpectatePlayer(player, target_player, previous_location);
                     return true;
                 }
-            } else {
-                player_list.addAll(Bukkit.getOnlinePlayers());
-                player_list.remove(player);
-                int player_count = player_list.size();
-                Random random = new Random();
-                Player target_player = player_list.get(random.nextInt(player_count));
-                //player.sendMessage(AlterColorCode(String.format("&e[관전] &a%s&f님이 걸렸습니다!", target_player.getDisplayName())));
-                Location previous_location = player.getLocation();
-                SpectatePlayer(player, target_player, previous_location);
-                return true;
             }
+        } else {
+            player.sendMessage(AlterColorCode("&c[ERROR] &f이 명령어를 사용할 권한이 없습니다."));
         }
         return false;
     }
